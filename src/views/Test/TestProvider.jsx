9a38ad18ff1055dc1fda531fb9daf90b72c8b0e7
sj <sjingye@163.com>
2019-08-09 15:34:26 +0800

    bind this

--- src/views/Test/TestProvider.jsx
+++ src/views/Test/TestProvider.jsx
@@ -27,10 +27,28 @@ function Toolbar(props) {
 }
 
 class ThemedButton extends React.Component {
+    constructor(props) {
+        super(props);
+        /* 
+        为了在回调中使用 `this`，这个绑定是必不可少的
+        
+        在整个class类中，所有的this指向都是class的实例，但是当回调函数被赋值后，其体内的this指向并不会继承原本this的指向，因为此刻触发该回调函数的并不是class的实例。所以你需要手动绑定该this指向。
+
+        该绑定的代码中，this代表的是回调函数对象，这句代码可以理解为：将该组件中的handleClick回调函数绑定到该组件上，实质上是将回调函数中的this指向该组件对象，然后将改变了执行上下文的回调函数，再重新替换掉原来的handleClick回调函数，那么此时的回调函数中的this指向的是类定义的组件。
+
+        */
+        this.handleClick = this.handleClick.bind(this)
+    }
     static contextType = ThemeContext
+    componentDidMount() {
+        this.handleClick()
+    }
+    handleClick() {
+        console.log(this)
+    }
     render() {
         return (
-            <button theme={this.context}>
+            <button theme={this.context} onClick={this.handleClick} style={{ width: '100%', height: '40px' }}>
                 测试provider
             </button>
         )
